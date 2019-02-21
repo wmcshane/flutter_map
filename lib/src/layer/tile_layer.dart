04484a1e14dd551ff85a43e392373f80cab22aa1
wozza <>
2019-02-21 12:08:47 +1100

    Corrected layer rendering.

--- lib/src/layer/tile_layer.dart
+++ lib/src/layer/tile_layer.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -40,6 +41,10 @@ class TileLayerOptions extends LayerOptions {
 
   final bool zoomReverse;
   final double zoomOffset;
+
+  /// number of offscreen tile columns/rows to render,
+  /// n tile rows top and bottom and n tile columns left and right.
+  final int offScreenTileBuffer;
 
   /// List of subdomains for the URL.
   ///
@@ -99,6 +104,7 @@ class TileLayerOptions extends LayerOptions {
       this.maxZoom = 18.0,
       this.zoomReverse = false,
       this.zoomOffset = 0.0,
+      this.offScreenTileBuffer = 0,
       this.additionalOptions = const <String, String>{},
       this.subdomains = const <String>[],
       this.keepBuffer = 2,
@@ -392,9 +398,11 @@ class _TileLayerState extends State<TileLayer> {
 
   Bounds _pxBoundsToTileRange(Bounds bounds) {
     var tileSize = getTileSize();
-    return Bounds(
-      bounds.min.unscaleBy(tileSize).floor(),
-      bounds.max.unscaleBy(tileSize).ceil() - CustomPoint(1, 1),
+    var halfTileSize = tileSize/2;
+    Point tileBuffer = Point(this.options.offScreenTileBuffer, this.options.offScreenTileBuffer);
+      return Bounds(
+      (bounds.min - halfTileSize).unscaleBy(tileSize).floor() - tileBuffer,
+      (bounds.max + halfTileSize).unscaleBy(tileSize).floor() + tileBuffer,
     );
   }
 
