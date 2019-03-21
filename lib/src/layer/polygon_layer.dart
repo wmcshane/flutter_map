3622108885bd8ce668c55bc18908e82852593d57
wozza <>
2019-03-22 10:25:53 +1100

    Added polygon culling, e.g. don't get rendered if they are't within the view.

--- lib/src/layer/polygon_layer.dart
+++ lib/src/layer/polygon_layer.dart
@@ -2,8 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/src/core/bounds.dart';
 import 'package:flutter_map/src/map/map.dart';
 import 'package:latlong/latlong.dart' hide Path;  // conflict with Path from UI
+import 'dart:math' as Math;
 
 class PolygonLayerOptions extends LayerOptions {
   final List<Polygon> polygons;
@@ -17,12 +19,77 @@ class Polygon {
   final Color color;
   final double borderStrokeWidth;
   final Color borderColor;
+  BoundingBox boundingBox;
   Polygon({
     this.points,
     this.color = const Color(0xFF00FF00),
     this.borderStrokeWidth = 0.0,
     this.borderColor = const Color(0xFFFFFF00),
-  });
+  }){
+    calcBoundingBox();
+  }
+
+  void calcBoundingBox()
+  {
+    BoundingBox bb = new BoundingBox();
+    for(LatLng point in points)
+    {
+      // convert lat lon to custom point stored as radians
+      num x = point.longitude * Math.pi / 180.0;
+      num y = point.latitude * Math.pi / 180.0;
+      CustomPoint cPoint = CustomPoint(x, y);
+
+      if(bb.minX == null || bb.minX > cPoint.x)
+      {
+        bb.minX = cPoint.x;
+      }
+
+      if(bb.minY == null || bb.minY > cPoint.y)
+      {
+        bb.minY = cPoint.y;
+      }
+
+      if(bb.maxX == null || bb.maxX < cPoint.x)
+      {
+        bb.maxX = cPoint.x;
+      }
+
+      if(bb.maxY == null || bb.maxY < cPoint.y)
+      {
+        bb.maxY = cPoint.y;
+      }
+    }
+
+    boundingBox = bb.getAsDegrees();
+  }
+}
+
+class BoundingBox
+{
+  double minX;
+  double maxX;
+  double minY;
+  double maxY;
+  BoundingBox({this.minX, this.maxX, this.minY, this.maxY});
+
+  // bounding box will be stored as latLng in radians instead of degrees
+  BoundingBox getAsDegrees()
+  {
+    BoundingBox latLngBB = BoundingBox (
+      minX: this.minX * 180 / Math.pi,
+      maxX: this.maxX * 180 / Math.pi,
+      minY: this.minY * 180 / Math.pi,
+      maxY: this.maxY * 180 / Math.pi,
+    );
+
+    return latLngBB;
+  }
+
+  @override
+  String toString()
+  {
+    return '$minX $maxX | $minY $maxY';
+  }
 }
 
 class PolygonLayer extends StatelessWidget {
@@ -45,9 +112,42 @@ class PolygonLayer extends StatelessWidget {
     return new StreamBuilder<int>(
       stream: stream, // a Stream<int> or null
       builder: (BuildContext context, _) {
+
+        var polygons = <Widget>[];
+        LatLngBounds screenBounds = map.bounds;
         for (var polygonOpt in polygonOpts.polygons) {
           polygonOpt.offsets.clear();
           var i = 0;
+
+
+
+          // cull polygons that aren't within the screen space
+          //check if polygon is within the view
+          //print('POLY Bounds: ${polygonOpt.boundingBox}');
+
+          LatLng minBound = LatLng(polygonOpt.boundingBox.minY, polygonOpt.boundingBox.minX);
+          LatLng maxBound = LatLng(polygonOpt.boundingBox.maxY, polygonOpt.boundingBox.maxX);
+
+//          var minPos = map.project(minBound);
+//          minPos = minPos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) - map.getPixelOrigin();
+//
+//          var maxPos = map.project(maxBound);
+//          maxPos = maxPos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) - map.getPixelOrigin();
+
+          if(!screenBounds.contains(minBound) && !screenBounds.contains(maxBound))
+          {
+            //print('Not drawing!');
+            // skip this polygon as it's offscreen
+            continue;
+          }
+          else
+          {
+            // print('\nScreen: $screenBounds');
+            // print('min: $minPos | max: $maxPos');
+            // print ('min: $minBound | max: $maxBound');
+          }
+
+          // convert polygon points to screen space
           for (var point in polygonOpt.points) {
             var pos = map.project(point);
             pos = pos.multiplyBy(map.getZoomScale(map.zoom, map.zoom)) - map.getPixelOrigin();
@@ -57,10 +157,8 @@ class PolygonLayer extends StatelessWidget {
             }
             i++;
           }
-        }
 
-        var polygons = <Widget>[];
-        for (var polygonOpt in this.polygonOpts.polygons) {
+          // add polygons to be rendered
           polygons.add(
             new CustomPaint(
               painter: new PolygonPainter(polygonOpt),
