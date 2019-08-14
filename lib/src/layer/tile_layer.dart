e16f7f458a3f4f19fae575ecfd3980446f338c0f
wozza <>
2019-08-14 15:50:11 +1000

    Added tile loading offset. Allows to load an extra tile to account for tiles not rendering when between zoom levels.

--- lib/src/layer/tile_layer.dart
+++ lib/src/layer/tile_layer.dart
@@ -42,6 +42,10 @@ class TileLayerOptions extends LayerOptions {
   final bool zoomReverse;
   final double zoomOffset;
 
+  /// number of offscreen tile columns/rows to render,
+  /// n tile rows top and bottom and n tile columns left and right.
+  final int offScreenTileBuffer;
+
   /// List of subdomains for the URL.
   ///
   /// Example:
@@ -100,6 +104,7 @@ class TileLayerOptions extends LayerOptions {
       this.maxZoom = 18.0,
       this.zoomReverse = false,
       this.zoomOffset = 0.0,
+      this.offScreenTileBuffer = 0,
       this.additionalOptions = const <String, String>{},
       this.subdomains = const <String>[],
       this.keepBuffer = 2,
@@ -393,9 +398,10 @@ class _TileLayerState extends State<TileLayer> {
 
   Bounds _pxBoundsToTileRange(Bounds bounds) {
     var tileSize = getTileSize();
+    var tileBuffer = CustomPoint(options.offScreenTileBuffer, options.offScreenTileBuffer);
     return Bounds(
-      bounds.min.unscaleBy(tileSize).floor(),
-      bounds.max.unscaleBy(tileSize).ceil() - CustomPoint(1, 1),
+      (bounds.min.unscaleBy(tileSize).floor()) - tileBuffer,
+      (bounds.max.unscaleBy(tileSize).ceil() - CustomPoint(1, 1)) + tileBuffer,
     );
   }
 
